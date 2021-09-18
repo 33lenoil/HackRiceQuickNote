280327f8483b5163e78cc975db69b2a4fdf9ac7a
magicCC02 <hanbinghbhb@gmail.com>
2021-09-18 10:15:32 -0500

    buttons

--- HR11_Font_Randomize_Popup/inject.js
+++ HR11_Font_Randomize_Popup/inject.js
@@ -1,7 +1,3 @@
-var elems = document.body.getElementsByTagName("*");
-
-for (const [key, value] of Object.entries(elems)) {
-    var newFontSize = Math.floor(Math.random() * 40);
-    value.style.fontSize = String(newFontSize) + 'px';
-}
+var ytplayer = document.getElementById("movie_player");
+ytplayer.pauseVideo();
 /* Testing */
--- HR11_Font_Randomize_Popup/popup.js
+++ HR11_Font_Randomize_Popup/popup.js
@@ -1,11 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-    var changeFontButton = document.getElementById('changeFont');
-    changeFontButton.addEventListener('click', function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            var activeTab = tabs[0];
-            chrome.tabs.executeScript(activeTab.id, {
-                file: 'inject.js'
-            });
+document.addEventListener('DOMContentLoaded', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.executeScript(activeTab.id, {
+            file: 'inject.js'
         });
+    });
+
+    var createNoteButton = document.getElementById('buttons').childNodes[1];
+    console.log(docFrame);
+    createNoteButton.addEventListener('click', function () {
+        var noteTaker = document.getElementById('docFrame');
+        var docURL = "https://docs.google.com/document/create"
+        noteTaker.src = docURL;
+
     }, false);
 }, false);
